3cac4a3c4cad23f74d254fbcbc12afdac7406bd9
Shubhankar-afk <61961170+Shubhankar-afk@users.noreply.github.com>
2020-09-03 11:04:14 +0530

    Add files via upload

---
+++ searchingElementsOfArray.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include <algorithm>
+using namespace std;
+
+//Not mine
+int case_insensitive_match(string s1, string s2) {
+   //convert s1 and s2 into lower case strings
+   transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+   transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+   if(s1==s2)
+      return 1; //The strings are same
+   return 0; //not matched
+}
+
+int main(){
+	string studentData[3][5]={{"Shubhankar", "Sneha", "Suvrat", "Rishabh", "Tanvince"}, {"30-CSE-19", "86-CSE-19", "02-CSE-19", "42-CSE-19", "82-CSE-19"}, {"A2", "A2", "A2", "A2", "A2"}};
+	int i, j, k;
+	string check;
+	findDetails: cout<<"Enter a name or roll number to find details: ";
+	getline(cin, check);
+	for(j=0; j<5; j++){
+		for(i=0; i<2; i++){
+			if(case_insensitive_match(check, studentData[i][j])){
+				break;
+			}
+			else{
+				continue;
+			}
+		}
+		if(case_insensitive_match(check, studentData[i][j])){
+			break;
+		}
+		if(j==4){
+			cout<<"Error 404";
+			exit(0);
+		}
+	}
+	cout<<studentData[0][j]<<" - "<<studentData[1][j]<<" - "<<studentData[2][j];
+/*	switch (i){
+		case 0:
+			cout<<studentData[0][j]<<" - "<<studentData[1][j]<<" - "<<studentData[2][j];
+		case 1:
+			cout<<studentData[0][j]<<" - "<<studentData[1][j]<<" - "<<studentData[2][j];
+	}*/
+
+/*
+	//Logic to print out all the details
+	for(j=0; j<5; j++){
+		for(i=0; i<3; i++){
+			if(i==1){
+				cout<<" - "<<studentData[i][j];
+				continue;
+			}
+			else if(i==2){
+				cout<<" - "<<studentData[i][j]<<endl;
+				break;
+			}
+			cout<<studentData[i][j];
+		}
+	}*/
+}
