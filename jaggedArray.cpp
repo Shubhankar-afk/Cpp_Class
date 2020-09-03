5a965886947d6e87683c5720ed6938bcd74b8099
Shubhankar-afk <61961170+Shubhankar-afk@users.noreply.github.com>
2020-09-03 23:49:07 +0530

    added comment

--- jaggedArray.cpp
+++ jaggedArray.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	int a, b, i, j;
 	char data[5][5][20]={"Shubhankar", "Sneha", "Suvrat", "Tanvince", "Rishabh"};
+	//Reads data and outputs it
 	for (i=0; i<5; i++){
 		cout<<data[i]<<endl;
 	}
